b096e5bb00607e7da4e77a34770feee10c45d6d2
ThomasViennet <viennet.t@gmail.com>
2023-07-05 12:02:47 +0200

    add SimilarityPercentage

--- index.test.js
+++ index.test.js
@@ -101,7 +101,7 @@ test('calculateSimilarityPercentage returns correct score', () => {
     const urls2 = ['url_A1', 'url_A2', 'url_B3', 'url_B4', 'url_B5'];
     const commonURLs = ['url_A1', 'url_A2'];
     const score = calculateSimilarityPercentage(urls1, urls2, commonURLs);
-    expect(Math.trunc(score)).toBe(74);
+    expect(score).toBe(74);
 });
 
 test('calculateSimilarityPercentage returns correct score', () => {
@@ -109,7 +109,7 @@ test('calculateSimilarityPercentage returns correct score', () => {
     const urls2 = ['url_A1', 'url_A2', 'url_A3', 'url_A4', 'url_A6'];
     const commonURLs = ['url_A1', 'url_A2', 'url_A3', 'url_A4'];
     const score = calculateSimilarityPercentage(urls1, urls2, commonURLs);
-    expect(Math.trunc(score)).toBe(98);
+    expect(score).toBe(98);
 });
 
 
@@ -118,20 +118,20 @@ test('calculateSimilarityPercentage returns correct score', () => {
     const urls2 = ['url_A1', 'url_A2', 'url_A3', 'url_A4', 'url_A5'];
     const commonURLs = ['url_A1', 'url_A2', 'url_A3', 'url_A4', 'url_A5'];
     const score = calculateSimilarityPercentage(urls1, urls2, commonURLs);
-    expect(Math.trunc(score)).toBe(100);
+    expect(score).toBe(100);
 });
 
 test('updateSimilarities updates the Similarities array correctly', () => {
     const queriesData = [
         { Query: 'A', URL: ['url_A1', 'url_A2', 'url_A3', 'url_A4', 'url_A5'], Similarities: [] },
-        { Query: 'B', URL: ['url_B1', 'url_B2', 'url_B3', 'url_A2', 'url_A1'], Similarities: [] },
+        { Query: 'B', URL: ['url_B1', 'url_B2', 'url_B3', 'url_A4', 'url_A5'], Similarities: [] },
         { Query: 'C', URL: ['url_A2', 'url_A1', 'url_C3', 'url_C4', 'url_C5'], Similarities: [] },
         { Query: 'D', URL: ['url_A1', 'url_A2', 'url_C3', 'url_C4', 'url_C5'], Similarities: [] }
     ];
     const queriesDataClone = JSON.parse(JSON.stringify(queriesData));
     const updatedQueriesData = updateSimilarities(queriesData, queriesDataClone);
     expect(updatedQueriesData[0].Similarities).toEqual(expect.arrayContaining([
-        { Query: 'B', URLs: ['url_A1', 'url_A2'], SimilarityScore: 13 }
+        { Query: 'B', URLs: ['url_A4', 'url_A5'], SimilarityScore: 5, SimilarityPercentage: 9 }
     ]));
 
 });
