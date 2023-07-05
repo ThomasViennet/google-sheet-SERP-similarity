602bace90490eb59136aa86742c28ebc80d6872b
ThomasViennet <viennet.t@gmail.com>
2023-07-05 19:37:33 +0200

    clean files

--- index.js
+++ index.js
@@ -35,6 +35,23 @@ function calculateSimilarityScore(urls1, urls2, commonURLs) {
     return score;
 }
 
+function calculateMaxSimilarityScore(urls1) {
+    let score = 0;
+    for (const url of urls1) {
+        const weight1 = urls1.length - urls1.indexOf(url);
+        const weight2 = urls1.length - urls1.indexOf(url);
+        score += weight1 * weight2;
+    }
+    return score;
+}
+
+function calculateSimilarityPercentage(urls1, urls2, commonURLs) {
+    const totalPossibleScore = calculateMaxSimilarityScore(urls1);
+    const score = calculateSimilarityScore(urls1, urls2, commonURLs);
+    const similarityPercentage = (score / totalPossibleScore) * 100;
+    return Math.trunc(similarityPercentage);
+}
+
 function updateSimilarities(queriesData, queriesDataClone) {
     for (const queryData of queriesData) {
         for (const queryDataClone of queriesDataClone) {
@@ -42,10 +59,12 @@ function updateSimilarities(queriesData, queriesDataClone) {
                 const commonURLs = getCommonURLs(queryData.URL, queryDataClone.URL);
                 if (commonURLs.length > 0) {
                     const similarityScore = calculateSimilarityScore(queryData.URL, queryDataClone.URL, commonURLs);
+                    const similarityPercentage = calculateSimilarityPercentage(queryData.URL, queryDataClone.URL, commonURLs);
                     queryData.Similarities.push({
                         'Query': queryDataClone.Query,
                         'URLs': commonURLs,
-                        'SimilarityScore': similarityScore
+                        'SimilarityScore': similarityScore,
+                        'SimilarityPercentage': similarityPercentage
                     });
                 }
             }
@@ -54,18 +73,44 @@ function updateSimilarities(queriesData, queriesDataClone) {
     return queriesData;
 }
 
-// Create a clone of queries data for comparison
+function getSimilarQueries(updatedQueriesData) {
+    const result = [];
+
+    for (const queryData of updatedQueriesData) {
+        if (queryData.Similarities.length > 0) {
+            // Sort the similarities in descending order of SimilarityPercentage
+            queryData.Similarities.sort((a, b) => b.SimilarityPercentage - a.SimilarityPercentage);
+            // Filter out the similarities where SimilarityPercentage is 0
+            const filteredSimilarities = queryData.Similarities.filter(similarity => similarity.SimilarityPercentage !== 0);
+
+            const similarQueries = filteredSimilarities.map(similarity => {
+                return `${similarity.Query} (${similarity.SimilarityPercentage}%)`;
+            });
+
+            // Only add an entry in the result if there are similar queries left after filtering
+            if (similarQueries.length > 0) {
+                result.push([similarQueries.join(", ")]);
+            }
+        } else {
+            result.push([""]);
+        }
+    }
+
+    return result;
+}
+
 const queriesData = createQueriesData(serp);
 const queriesDataClone = JSON.parse(JSON.stringify(queriesData));
-
-// Update the Similarities for each query in the queries data array
 const updatedQueriesData = updateSimilarities(queriesData, queriesDataClone);
+const similarQueriesArray = getSimilarQueries(updatedQueriesData);
 
-// console.log(JSON.stringify(updatedQueriesData, null, 2));
 
 module.exports = {
     createQueriesData,
     getCommonURLs,
     calculateSimilarityScore,
+    calculateMaxSimilarityScore,
+    calculateSimilarityPercentage,
     updateSimilarities,
+    getSimilarQueries,
 };
