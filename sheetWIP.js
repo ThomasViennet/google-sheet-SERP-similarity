af4ef01db6789713283d45a548a8a565750f12ee
ThomasViennet <viennet.t@gmail.com>
2023-07-06 12:06:06 +0200

    process cleaning for google sheet

--- sheetWIP.js
+++ sheetWIP.js
@@ -1,44 +1,81 @@
-// fixtures for WIP
-const fixtures = require('./fixtures.js');
-serp = fixtures.serp;
-dataSelected = fixtures.dataSelected;
+// Start for Google Sheet
+// Otherwise, fetch the data using Google Sheet functions
+function onOpen(e) {
+    SpreadsheetApp.getUi()
+        .createMenu('SEO')
+        .addItem('Calculer la similarité', 'calculateSimilarity')
+        .addToUi();
+}
 
-// data from Google Sheet
-// const sheet = SpreadsheetApp.getActiveSpreadsheet();
-// const selection = sheet.getSelection().getActiveRange();
-// const dataSelected = selection.getValues();
-// const serp = sheet.getSheetByName("SERP").getRange("A2:B").getValues();
-// const activeSheet = selection.getSheet(); // The sheet user is selecting
+function calculateSimilarity() {
+    const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+    const serpSheet = spreadsheet.getSheetByName("SERP");
 
-// const columnIndex = getColumnIndex(activeSheet, "Cannibalisation");
-// const selectedColumnIndex = getSelectedColumnIndex(selection);
+    let serp = serpSheet.getRange("A2:B" + serpSheet.getLastRow()).getValues();
+    let dataSelected = getColumnData("Requête");
+    let targetUrl = getColumnData("URL cible");
+    var statusData = getColumnData("État");
 
-// Create a clone of queries data for comparison
-const queriesData = createQueriesData(serp, dataSelected);
-const queriesDataClone = JSON.parse(JSON.stringify(queriesData));
+    const queriesData = createQueriesData(serp, dataSelected, targetUrl);
+    const queriesDataClone = JSON.parse(JSON.stringify(queriesData));
+    const updatedQueriesData = updateSimilarities(queriesData, queriesDataClone);
+    const similarQueriesArray = getSimilarQueries(updatedQueriesData);
 
-// Update the Similarities for each query in the queries data array
-updateSimilarities(queriesData, queriesDataClone, 'activeSheet', 'selectedColumnIndex', 'columnIndex'); // 'activeSheet', 'selectedColumnIndex', 'columnIndex' are for Google Sheet)
+    updateColumnData("Cannibalisation", getSimilarQueries(updatedQueriesData));
+}
 
-function getColumnIndex(sheet, columnName) {
-    const headers = sheet.getRange(1, 1, 1, sheet.getLastColumn()).getValues()[0];
-    let index = headers.indexOf(columnName);
-    if (index === -1) {
-        index = headers.length;  // Index of new column
-        sheet.getRange('A1').offset(0, index).setValue(columnName);  // Set value in new column
+function getColumnData(columnName) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    var headers = sheet.getRange("1:1").getValues()[0];
+    var columnIndex;
+
+    for (var i = 0; i < headers.length; i++) {
+        if (headers[i] === columnName) {
+            columnIndex = i + 1;
+            break;
+        }
     }
-    return index + 1; // Adjust for 0-index to 1-index
+
+    if (columnIndex === undefined) {
+        throw new Error(`No "${columnName}" column found.`);
+    }
+
+    var dataRange = sheet.getRange(2, columnIndex, sheet.getLastRow() - 1);
+    var data = dataRange.getValues();
+
+    return data;
 }
 
-function getSelectedColumnIndex(selection) {
-    return selection.getCell(1, 1).getColumn();
+function updateColumnData(columnName, updatedData) {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    var headers = sheet.getRange("1:1").getValues()[0];
+    var columnIndex;
+
+    for (var i = 0; i < headers.length; i++) {
+        if (headers[i] === columnName) {
+            columnIndex = i + 1;
+            break;
+        }
+    }
+
+    if (columnIndex === undefined) {
+        throw new Error(`No "${columnName}" column found.`);
+    }
+
+    var dataRange = sheet.getRange(2, columnIndex, sheet.getLastRow() - 1);
+    dataRange.clearContent();
+
+    var outputRange = sheet.getRange(2, columnIndex, updatedData.length);
+    outputRange.setValues(updatedData);
 }
 
-function createQueriesData(serp, dataSelected) {
+// END for Google Sheet
+
+function createQueriesData(serp, dataSelected, targetUrl) {
     const queriesData = [];
-    for (const data of dataSelected) {
-        const URLs = serp.filter(el => el[0] === data[0]).map(el => el[1]);
-        queriesData.push({ 'Query': data[0], 'URL': URLs, 'Similarities': [] });
+    for (let i = 0; i < dataSelected.length; i++) {
+        const URLs = serp.filter(el => el[0] === dataSelected[i][0]).map(el => el[1]);
+        queriesData.push({ 'Query': dataSelected[i][0], 'URL': URLs, 'Similarities': [], 'TargetUrl': targetUrl[i][0] });
     }
     return queriesData;
 }
@@ -55,38 +92,59 @@ function calculateSimilarityScore(urls1, urls2, commonURLs) {
         const weight2 = urls2.length - urls2.indexOf(url);
         score += weight1 * weight2;
     }
-
-    const maxScore1 = urls1.reduce((sum, _, i) => sum + (i + 1) ** 2, 0);
-    const maxScore2 = urls2.reduce((sum, _, i) => sum + (i + 1) ** 2, 0);
-    const maxScore = Math.min(maxScore1, maxScore2);
-
-    const similarityPercentage = score / maxScore * 100;
-
-    return { score, similarityPercentage };
+    return score;
 }
 
-function updateSimilarities(queriesData, queriesDataClone, sheet, selectedColumnIndex, cannibalisationColumnIndex) {
+function calculateMaxSimilarityScore(urls1) {
+    let score = 0;
+    for (const url of urls1) {
+        const weight1 = urls1.length - urls1.indexOf(url);
+        const weight2 = urls1.length - urls1.indexOf(url);
+        score += weight1 * weight2;
+    }
+    return score;
+}
+
+function calculateSimilarityPercentage(urls1, urls2, commonURLs) {
+    const totalPossibleScore = calculateMaxSimilarityScore(urls1);
+    const score = calculateSimilarityScore(urls1, urls2, commonURLs);
+    const similarityPercentage = (score / totalPossibleScore) * 100;
+    return Math.trunc(similarityPercentage);
+}
+
+function updateSimilarities(queriesData, queriesDataClone) {
     for (const queryData of queriesData) {
-        let similarityList = [];
         for (const queryDataClone of queriesDataClone) {
             if (queryData.Query !== queryDataClone.Query) {
                 const commonURLs = getCommonURLs(queryData.URL, queryDataClone.URL);
                 if (commonURLs.length > 0) {
-                    const { score, similarityPercentage } = calculateSimilarityScore(queryData.URL, queryDataClone.URL, commonURLs);
+                    const similarityScore = calculateSimilarityScore(queryData.URL, queryDataClone.URL, commonURLs);
+                    const similarityPercentage = calculateSimilarityPercentage(queryData.URL, queryDataClone.URL, commonURLs);
                     queryData.Similarities.push({
                         'Query': queryDataClone.Query,
                         'URLs': commonURLs,
-                        'SimilarityScore': score,
-                        'SimilarityPercentage': similarityPercentage
+                        'SimilarityScore': similarityScore,
+                        'SimilarityPercentage': similarityPercentage,
+                        'TargetUrl': queryDataClone.TargetUrl
                     });
-                    similarityList.push(queryDataClone.Query + ' ' + similarityPercentage);
                 }
             }
         }
-
-        if (similarityList.length > 0) {
-            const rowIndex = queriesData.indexOf(queryData) + 2; // Adjust for headers and 0-index
-            // sheet.getRange(rowIndex, cannibalisationColumnIndex).setValue(similarityList.join(', '));// for Google Sheet
-        }
     }
+    return queriesData;
+}
+
+function getSimilarQueries(updatedQueriesData) {
+    return updatedQueriesData.map(queryData => {
+        const nonZeroSimilarsWithDifferentTargetUrl = queryData.Similarities
+            .filter(similarity => similarity.SimilarityPercentage !== 0 && similarity.TargetUrl !== queryData.TargetUrl)
+            .sort((a, b) => b.SimilarityPercentage - a.SimilarityPercentage);
+
+        if (nonZeroSimilarsWithDifferentTargetUrl.length === 0) {
+            return [""];
+        }
+
+        const similarQueries = nonZeroSimilarsWithDifferentTargetUrl.map(similarity => `${similarity.Query} (${similarity.SimilarityPercentage}%)`);
+        return [similarQueries.join(", ")];
+    });
 }
